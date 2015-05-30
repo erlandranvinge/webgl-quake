3518d55082547f10f5490538eace09284ed6aa2d
erland <erland.ranvinge@gmail.com>
2015-05-30 20:38:40 +0200

    Initial commit

---
+++ js/gl/gl.js
@@ -0,0 +1,11 @@
+var test = require('/lib/gl-matrix-min');
+
+
+var gl = function() {};
+
+gl.init = function(id) {
+
+};
+
+module.exports = exports = gl;
+
---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+var gl = require('./gl/gl');
+
+
+
+console.log('Hello!');
+
