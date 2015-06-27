a83ce3cac4e8d1f89d2a0d1b7ffe204744df862a
erland <erland.ranvinge@gmail.com>
2015-06-27 15:18:37 +0200

    Support local server when installing

--- js/installer/installer.js
+++ js/installer/installer.js
@@ -4,6 +4,7 @@ var lh4 = require('lib/lh4.js');
 
 var Installer = function() {
     this.dialog = new Dialog('dialog');
+    this.isLocal = window.location.hostname.indexOf('localhost') !== -1;
 };
 
 Installer.crossOriginProxyUrl = 'http://crossorigin.me/';
@@ -13,18 +14,28 @@ Installer.mirrors = [
 
 Installer.prototype.start = function(done) {
     this.dialog.setCaption('Downloading shareware version of Quake (quake106.zip)...');
+    var url = this.isLocal ?
+        'data/quake106.zip' :
+        Installer.crossOriginProxyUrl + Installer.mirrors[0];
 
-    var url = Installer.crossOriginProxyUrl + Installer.mirrors[0];
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
     xhr.overrideMimeType('text/plain; charset=x-user-defined');
-    xhr.responseType = 'arraybuffer';
+   // xhr.responseType = 'arraybuffer';
+
+    var self = this;
+
+    xhr.onreadystatechange = function() {
+        self.dialog.setCaption('HATT...', this.readyState);
+    };
+
     xhr.onload = function(e) {
         if (this.status === 200) {
+
             var blob = new Blob([this.response]);
             zip.createReader(new zip.BlobReader(blob), function(reader) {
                 reader.getEntries(function(entries) {
-
+                     console.log('ENTRY', entries[0]);
                 });
             });
         }
