c004ca17a5b0a09092735d5eb9a1f89a8fa60bee
Erland Ranvinge <erland.ranvinge@gmail.com>
2017-10-19 20:38:11 +0200

    Update installer.js

--- js/installer/installer.js
+++ js/installer/installer.js
@@ -10,9 +10,9 @@ var Installer = function() {
 };
 
 Installer.localUrl = 'data/quake106.zip';
-Installer.crossOriginProxyUrl = 'http://crossorigin.me/';
+Installer.crossOriginProxyUrl = 'http://crossorigin.herokuapp.com/';
 Installer.mirrors = [
-    'http://www.gamers.org/pub/games/quake/idstuff/quake/quake106.zip'
+    'http://ftp.mancubus.net/pub/idgames/idstuff/quake/quake106.zip'
 ];
 
 Installer.prototype.error = function(message) {
