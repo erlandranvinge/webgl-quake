e93afeffe83c7fb22ebe04c37dba04b747ce2cd7
erland <erland.ranvinge@gmail.com>
2020-01-13 12:04:17 +0100

    upgraded gulp and replaced node with yarn

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,56 @@ var browserify = require('gulp-browserify');
 var watch = require('gulp-watch');
 var connect = require('gulp-connect');
 
+/*
 function swallowError (error) {
    console.log(error.toString());
     this.emit('end');
 }
+*/
 
+gulp.task('connect', () => 
+    connect.server({root: '', livereload: true})
+);
+
+gulp.task('watch', () => {
+    const options = {
+        paths: ['./js'],    
+        insertGlobals: true,
+        debug: true
+    };
+
+    watch('js/**/*.js', () => 
+        gulp.src('js/quake.js')
+            .pipe(browserify(options))
+            .pipe(gulp.dest('./'))
+            .pipe(connect.reload()));
+});
+
+gulp.task('default', gulp.series(['connect', 'watch']));
+
+/*
+gulp.task('default', function (callback) {
+  runSequence(['connect', ''],
+    callback
+  )
+})*/
+
+
+
+
+/*
 gulp.task('connect', function() {
     connect.server({
         root: '',
         livereload: true
     });
 });
+*/
 
-gulp.task('watch', function() {
-    watch('js/**/*.js', function() {
+//gulp.task('watch', function() {
+//    watch('js/**/*.js', function() {
 
-        var options = {
+  /*      var options = {
             paths: ['./js'],
             insertGlobals: true,
             //insertGlobalVars: 'Buffer',
@@ -34,4 +68,4 @@ gulp.task('watch', function() {
     });
 });
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['connect', 'watch']);*/
